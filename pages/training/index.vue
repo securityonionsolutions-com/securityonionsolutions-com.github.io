2d4e75be4f798f4d8bedec51b187eccd476df858
Jason Ertel <jason.ertel@securityonionsolutions.com>
2020-10-16 12:57:20 -0400

    Rename images for training

--- pages/training/index.vue
+++ pages/training/index.vue
@@ -60,7 +60,7 @@
           <template #graphic>
             <CircleGraphic
               class="shadow-xl"
-              file-name="online1.jpg"
+              file-name="training1.jpg"
               :size="20"
             />
           </template>
@@ -99,7 +99,7 @@
           <template #graphic>
             <CircleGraphic
               class="shadow-xl"
-              file-name="online2.jpg"
+              file-name="training2.jpg"
               :padding="false"
               :size="20"
             />
