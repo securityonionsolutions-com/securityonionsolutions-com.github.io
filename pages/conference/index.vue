be3e7fea43de51eb5f1d9b7e9fc02453bb68ec7d
William Wernert <william.wernert@securityonionsolutions.com>
2021-08-02 09:58:37 -0400

    Add link to SOC'21 registration
    * To go live 12PM EST 08-02-21

--- pages/conference/index.vue
+++ pages/conference/index.vue
@@ -16,7 +16,9 @@
           </div>
         </div>
         <div class="py-4 -px-4 text-base">
-          Mark your calendar! This year's Security Onion Conference is currently scheduled to be held in person! Registration will open August 2 <br> at Noon Eastern Time.
+          Mark your calendar! This year's Security Onion Conference <br> is currently scheduled to be held in person!
+          <br><br>
+          <span class="font-bold">Registration is NOW OPEN <a href="https://socaugusta2021.eventbrite.com/" class="text-so-blue cursor-pointer hover:underline">here</a></span>
         </div>
       </template>
     </SubHero>
