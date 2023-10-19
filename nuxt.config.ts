228a3b28b555a27afb9526a8a55010dda3e64c2e
Jason Ertel <jason.ertel@securityonionsolutions.com>
2023-10-19 11:17:26 -0400

    fix tag

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ export default defineNuxtConfig({
   runtimeConfig: {
   	public: {
     	sitekey: process.env.NODE_ENV === 'development' ? '6LcfdNAZAAAAAK7JI_iWN86_sBkE0mtu4gtLj5Wz' : '6LeRbtAZAAAAAAUXLyY1xJHUdssTwSp8MloDVZ2Y',
-    	gtag: 'AW-11380471320'
+    	gtag: 'G-W2EGS9J4H9'
     }
   },
   modules: [
