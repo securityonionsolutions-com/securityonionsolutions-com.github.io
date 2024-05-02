3fa34e88f5f8f484c792eea8818d3eb529629857
Jason Ertel <jason.ertel@securityonionsolutions.com>
2024-05-02 09:52:06 -0400

    update privacy policy

--- pages/privacy/index.vue
+++ pages/privacy/index.vue
@@ -7,74 +7,134 @@
       </template>
       <template #body>
         <div class="text-center">
-          Updated on 07/14/2021
+          Updated on 05/02/2024
         </div>
       </template>
     </SubHero>
     <ContentSection id="overview" :alternate="true">
       <div class="text-xl px-6 xs:px-12 lg:px-32">
-        <div class="text-3xl mt-8 mb-3">
-          Overview
+        <div class="text-3xl mt-8">
+          Introduction
         </div>
-        Every time you use our products or services some information about you may be collected. Please review this Privacy Policy in its entirety.
-        <div class="text-3xl mt-8 mb-3">
+        <p class="mt-4">
+          Security Onion Solutions, LLC (<b>"Company"</b> or <b>"We"</b>) respect your privacy and are committed to protecting it through our compliance with this policy.
+        </p>
+        <p class="mt-4">
+          This policy describes the types of information we may collect from you or that you may provide when you visit the website securityonionsolutions.com (our <b>"Website"</b>) or use our products and services (<b>“Products”</b>) and our practices for collecting, using, maintaining, protecting, and disclosing that information.
+        </p>
+        <p class="mt-4">
+          This policy applies to information we collect:
+        </p>
+        <ul class="privacy-list">
+          <li>On this Website;</li>
+          <li>In email, text, and other electronic messages between you and this Website; or</li>
+          <li>When you use our Products.</li>
+        </ul>
+        <p class="mt-4">
+          It does not apply to information collected by:
+        </p>
+        <ul class="privacy-list">
+          <li>Us offline or through any other means, including on any other website operated by Company or any third party; or </li>
+          <li>Any third party, including through any application or content (including advertising) that may link to or be accessible from or through the Website.</li>
+        </ul>
+        <p class="mt-4">
+          Please read this policy carefully to understand our policies and practices regarding your information and how we will treat it. If you do not agree with our policies and practices, your choice is not to use our Website or our Products. This policy may change from time to time. Please check the policy periodically for updates.
+        </p>
+
+        <div class="text-3xl mt-8">
+          Children Under the Age of 18
+        </div>
+        <p class="mt-4">
+          Our Website is not intended for children under 18 years of age. No one under age 18 may provide any personal information to or on the Website. We do not knowingly collect personal information from children under 18. If you are under 18, do not provide any information about yourself to us, including your name, address, telephone number, email address, or any screen name or user name you may use. If we learn we have collected or received personal information from a child under 18 without verification of parental consent, we will delete that information. If you believe we might have any information from or about a child under 18, please contact us at <a href="mailto:legal@securityonionsolutions.com">legal@securityonionsolutions.com</a>.
+        </p>
+
+        <div class="text-3xl mt-8">
           Collected Information
         </div>
-        When using our products or services, some non-personally identifiable information may be collected. Examples of this information include the internet address from which you are connecting and the type and version of the operating system and web browser you are using.
         <p class="mt-4">
-          When using our products or services we may also collect personally identifiable information. Examples of this information include your name, username, and email address.
+          When using our Website or our Products, some information may be automatically collected. Information that is automatically collected includes the internet address from which you are connecting, the type and version of the operating system and web browser you are using, and information about your visit to the Website, including length of visit and pages you visit. Such information does not automatically identify you personally, although if combined with other personally identifiable information it may be used to identify you personally. 
         </p>
         <p class="mt-4">
-          When registering for our products or services via a third-party, such as an online marketplace, the third-party provider may choose to share personally identifiable information with us. Examples of this information include your name, email address, physical mailing address, etc.
+          When using our Website or our Products, we may also collect personally identifiable information that you provide to us, including your name and email address.
         </p>
         <p class="mt-4">
-          Both the non-personally identifiable and personally identifiable information are together referred to as 'collected information' in the remainder of this privacy policy.
+          When registering for our Products via a third party, such as an online marketplace, the third-party provider may choose to share personally identifiable information with us. Examples of this information include your name, email address, or any other information you provide to such third party.
         </p>
-        <div class="text-3xl mt-8 mb-3">
+        <p class="mt-4">
+          Both the automatically collected information and information that you provide to us are together referred to as 'collected information' in the remainder of this privacy policy. 
+        </p>
+
+        <div class="text-3xl mt-8">
           Usage
         </div>
-        We use the collected information to improve our products and websites and to help us provide a better user experience. This information, when combined with the information of our other users, in the aggregate, will allow us to analyze product and service usage patterns.
         <p class="mt-4">
-          We may use the collected information to contact you. This could occur for many reasons, such as when we respond to your product or website inquiry. This could also occur when we need to notify you about product changes, system outages, or security-related events that we believe may be of interest to you.
+          We use the automatically collected information to improve our products and websites and to help us provide a better user experience. This information, when combined with the information of our other users, in the aggregate, will allow us to analyze product and service usage patterns.
         </p>
-        <div class="text-3xl mt-8 mb-3">
+        <p class="mt-4">
+          We may use the information you provide us to contact you. This could occur for many reasons, such as when we respond to your Product or Website inquiry. This could also occur when we need to notify you about Product changes, system outages, or security-related events that we believe may be of interest to you.
+        </p>
+        <p class="mt-4">
+          We may share your information with our service providers who support our business, solely for the purpose of supporting our business.
+        </p>
+
+        <div class="text-3xl mt-8">
           Disclosure
         </div>
-        We may disclose this collected information in response to legal processes, for example, when directed by a court order or subpeona. This information may also be disclosed to law enforcement officials when requested, or when we feel doing so would assist in the prevention of illegal activities, physical harm to people or animals, or violations to our product licenses or terms of use.
         <p class="mt-4">
-          We may transfer this collected information if one or more of our business units is acquired by, sold to, or merged with another company. However, that company must also protect this collected information in a manner that is consistent with this Privacy Policy.
+          We may disclose the collected information in response to legal processes, for example, when directed by a court order or subpoena. This information may also be disclosed to law enforcement officials when requested, or when we feel doing so would assist in the prevention of illegal activities, physical harm to people or animals, or violations to our product licenses or terms of use. 
         </p>
         <p class="mt-4">
-          We will not transfer or disclose your personally identifiable information to other companies for their marketing or other business ventures, except as when noted otherwise in this Privacy Policy.
+          We may transfer this collected information if one or more of our business units is acquired by, sold to, or merged with another company.
+        </p>
+        <p class="mt-4">
+          We will not transfer or disclose your personally identifiable information to other companies for their marketing or other business ventures, or use automatically collected information to identify you personally, except as when noted otherwise in this Privacy Policy. 
         </p>
         <p class="mt-4">
           We take appropriate measures at preventing unwanted disclosure of this collected information.
         </p>
-        <div class="text-3xl mt-8 mb-3">
+
+        <div class="text-3xl mt-8">
           Cookies
         </div>
-        Our products and services use "cookies" to store collected information onto your computer. This collected information may contain both non-personally identifiable information and personally identifiable information. For example, this information could include your email address, preferences, and other information that our products and services need to provide an improved user experience.
         <p class="mt-4">
-          You can configure your browser to accept all cookies, reject all cookies, or notify you when a cookie is set. This site provides instructions for adjusting cookie preferences for common browsers:
+          Our Website and Products use "cookies" to store collected information onto your computer. This collected information may contain both non-personally identifiable information and personally identifiable information. For example, this information could include your email address, preferences, and other information that we use to provide an improved user experience. 
         </p>
         <p class="mt-4">
+          You can configure your browser to accept all cookies, reject all cookies, or notify you when a cookie is set. This site provides instructions for adjusting cookie preferences for common browsers: 
+        </p>
+        <p class="mt-2">
           <a href="https://www.aboutcookies.org/">How to control cookies</a>
+        </p>
+        <p class="mt-4">
+          Note that rejecting cookies in our products may prevent proper use of those products.
         </p>
         <p class="mt-4">
           If your browser is not listed on the site above, check your browser's "Help" menu to learn how to change your cookie preferences.
         </p>
-        <div class="text-3xl mt-8 mb-3">
+
+        <div class="text-3xl mt-8">
+          Data Security
+        </div>
+        <p class="mt-4">
+          We have implemented measures designed to secure your personal information from accidental loss and from unauthorized access, use, alteration, and disclosure. Information you provide to us is stored at Amazon Web Services (“AWS”) or in Google Analytics. You may read about the AWS security measures at <a href="https://aws.amazon.com/privacy/">https://aws.amazon.com/privacy/</a>. You may read about Google Analytics’ security measures at <a href="https://policies.google.com/privacy?hl=en">https://policies.google.com/privacy?hl=en</a>.
+        </p>
+        <p class="mt-4">
+          Unfortunately, the transmission of information via the internet is not completely secure. Although we do our best to protect your personal information, we cannot guarantee the security of your personal information transmitted to our Website. Any transmission of personal information is at your own risk. We are not responsible for circumvention of any privacy settings or security measures contained on the Website. 
+        </p>
+
+        <div class="text-3xl mt-8">
           Third-Party Sites
         </div>
-        Our products or services contain links to third-party services. For example, third-party links can show up in your web browser and appear to be part of our website, however, they are not ours and the content provided by those links do not originate from our servers. When you transition to third-party websites, this privacy policy no longer applies.
-        <div class="text-3xl mt-8 mb-3">
-          Changes to this Privacy Policy
-        </div>
-        We may update our privacy policy from time to time. We will post the updated version in this section of the website.
-        <div class="text-3xl mt-8 mb-3">
+        <p class="mt-4">
+          Our Website and Products may contain links to third-party services; however, they are not ours and the content provided by those links do not originate from our servers. When you transition to third-party websites, this privacy policy no longer applies.
+        </p>
+
+        <div class="text-3xl mt-8">
           Contact Us
         </div>
-        If you have any questions about this policy, the practices of this site, or your dealings with this website, please <a style="cursor: pointer" @click="sos.showContactModal({text: 'I have a question about the privacy policy.', source: 'footer_icon'})">contact us</a>.
+        <p class="mt-4">
+          If you have any questions about this policy, the practices of this Website or our Products, or your dealings with this Website or our Products, please contact us at <a href="mailto:legal@securityonionsolutions.com">legal@securityonionsolutions.com</a>.
+        </p>
       </div>
     </ContentSection>
   </div>
@@ -103,5 +163,10 @@ export default {
 }
 a {
   text-decoration: underline;
+}
+.privacy-list {
+  list-style: disc;
+  margin-left: 2em;
+  margin-top: 1em;
 }
 </style>
