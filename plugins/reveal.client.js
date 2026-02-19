53135d6c91ecb1cb385aaabd4b08514228beadf3
Jason Ertel <jason.ertel@securityonionsolutions.com>
2026-02-18 19:27:17 -0500

    modernize

---
+++ plugins/reveal.client.js
@@ -0,0 +1,32 @@
+export default defineNuxtPlugin((nuxtApp) => {
+  nuxtApp.vueApp.directive('reveal', {
+    mounted(el, binding) {
+      const observer = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            el.classList.add('active');
+            if (binding.modifiers.once) {
+              observer.unobserve(el);
+            }
+          } else {
+            if (!binding.modifiers.once) {
+              el.classList.remove('active');
+            }
+          }
+        });
+      }, {
+        threshold: 0,
+        rootMargin: '0px 0px 0px 0px'
+      });
+
+      el.classList.add('reveal');
+      observer.observe(el);
+      el._revealObserver = observer;
+    },
+    unmounted(el) {
+      if (el._revealObserver) {
+        el._revealObserver.disconnect();
+      }
+    }
+  });
+});
