84ff3879267e41307843ebe03dc30b14b431289e
j-bernal <john.bernal@securityonionsolutions.com>
2026-08-28 10:40:39 -0400

    Updated Registration Guide document name to fix link.

--- pages/certification/index.vue
+++ pages/certification/index.vue
@@ -19,6 +19,7 @@
       </template>
     </SubHero>
     <content-section :alternate="true">
+    
       <div class="px-6 xs:px-12 lg:px-32">
         <div v-reveal class="text-2xl md:text-4xl text-center font-semibold reveal-scale">
           The SOCP exam validates that you have the knowledge and skills to properly investigate alerts, hunt for adversaries,
@@ -86,7 +87,7 @@
                 Schedule exam
               </so-button>
               <div class="text-sm -mt-4 mb-3 hover:underline">
-                <a href="/files/socp-registration-guide-v32.pdf">
+                <a href="/files/socp-registration-guide-v3_2.pdf">
                   <div class="flex flex-row justify-center content-center">
                     <div>
                       Registration Guide
